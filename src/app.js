5656b6261df381f0f1c608c1e4d23e5ca1f42772
charisanag <xaris.anag@gmail.com>
2021-12-03 17:21:18 +0000

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const geocode = require('./utils/geocode')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectory = path.join(__dirname, '../public')
@@ -67,14 +68,6 @@ app.get('/weather', (req, res) => {
                 location: location,
                 address: req.query.address
             })
-            // res.render(
-            //     'index', {
-            //         title: 'Current Weather',
-            //         forecast: forecastData,
-            //         location: location,
-            //         address: req.query.address
-            //     }
-            // )
         })
     }) 
   
@@ -110,6 +103,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
